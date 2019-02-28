d4316ab995d7ed090ad0ca946adcb8eab8085501
youngchil <yunziri@gmail.com>
2019-02-28 12:17:56 +0900

    fix genesis

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -112,7 +112,7 @@ public:
         nMinerThreads = 0;
         nTargetTimespan = 1 * 60; // GIANT: 1 day
         nTargetSpacing = 1 * 60;  // GIANT: 2 minute
-        nLastPOWBlock = 71400;
+        nLastPOWBlock = 100;
         nMaturity = 10;
         nMasternodeCountDrift = 20;
 	nMasternodeColleteralLimxDev = 1000; //Params().MasternodeColleteralLimxDev()
