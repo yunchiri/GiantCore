65a59e7c4ae8eff3f8654ea052b145061fc8fbd3
youngchil <yunziri@gmail.com>
2019-03-02 17:17:50 +0900

    fix

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -109,7 +109,7 @@ public:
         nMinerThreads = 0;
         nTargetTimespan = 1 * 60; // GIANT: 1 day
         nTargetSpacing = 1 * 60;  // GIANT: 2 minute
-        nLastPOWBlock = 150;
+        nLastPOWBlock = 300;
         nMaturity = 10;
         nMasternodeCountDrift = 20;
 	    nMasternodeColleteralLimxDev = 1000; //Params().MasternodeColleteralLimxDev()
